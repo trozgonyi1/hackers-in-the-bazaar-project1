f92c51c171f73c9792768d1bf5c18c8626192948
Francis Drake <fdrake@snd.edu>
2025-09-16 14:26:15 -0400

    beginnings of fast.c

--- fast.c
+++ fast.c
@@ -1,1 +1,44 @@
-// hopefully fast one
+// Hopefully fast one
+
+// Testing out the stack approach to simulate recursion
+// In the process of converting c++ logic to c logic
+
+#include <stdio.h>
+
+long long int Ackermann(long long m, long long n)
+    {
+        // declare stack
+        Stack<BigInteger> stack = new Stack<BigInteger>();
+
+        // push m
+        stack.Push(m);
+
+        while(stack.Count != 0)
+        {
+            m = stack.Pop();
+
+        // recurse down 
+        skipStack:
+            // pattern for the first few m's
+            if(m == 0)
+                n = n + 1;
+            else if(m == 1)
+                n = n + 2;
+            else if(m == 2)
+                n = n * 2 + 3;
+            else if(n == 0)
+            {
+                --m;
+                n = 1;
+                goto skipStack;
+            }
+            else
+            {
+                stack.Push(m - 1);
+                --n;
+                goto skipStack;
+            }
+        }
+        return n;
+    }
+s
