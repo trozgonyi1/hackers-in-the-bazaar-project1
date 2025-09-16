9a7970dd4e23321d52a4dfc8386682dca14e5c81
Francis Drake <fdrake@snd.edu>
2025-09-16 14:43:02 -0400

    c rough draft fast.c

--- fast.c
+++ fast.c
@@ -4,41 +4,34 @@
 // In the process of converting c++ logic to c logic
 
 #include <stdio.h>
+#include "stack.h"
 
 long long int Ackermann(long long m, long long n)
     {
-        // declare stack
-        Stack<BigInteger> stack = new Stack<BigInteger>();
+        // stack creation
+        Stack* stack = createStack(1000000);
+        push(stack, m)
 
-        // push m
-        stack.Push(m);
+        while(!not_empty(stackx)) {
+            pop(stack, &m);
 
-        while(stack.Count != 0)
-        {
-            m = stack.Pop();
-
-        // recurse down 
-        skipStack:
-            // pattern for the first few m's
-            if(m == 0)
-                n = n + 1;
-            else if(m == 1)
-                n = n + 2;
-            else if(m == 2)
-                n = n * 2 + 3;
-            else if(n == 0)
-            {
-                --m;
-                n = 1;
-                goto skipStack;
-            }
-            else
-            {
-                stack.Push(m - 1);
-                --n;
-                goto skipStack;
-            }
+        if (m == 0)
+            n = n + 1;
+        else if (m == 1)
+            n = n + 2;
+        else if (m == 2)
+            n = (n * 2) + 3;
+        else if (n == 0) {
+            --m;
+            n = 1;
+            goto skipStack
+        } else {
+            --n;
+            push(stack, m - 1)
+            goto skipStack
         }
+        }
+
         return n;
     }
 s
